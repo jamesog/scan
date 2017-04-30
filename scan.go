84ed3cd532a5269e071bd7273b042187adb5924d
James O'Gorman <james@netinertia.co.uk>
2017-04-30 21:03:37 +0100

    Initial import
    scan.go provides a web server providing a UI for viewing scan results
    and an endpoint accepting JSON data from Masscan.
    
    This also vendors Bootstrap 3 and Typeahead.js and a CSS file to fix the
    style on the Typeahead dropdown.

---
+++ scan.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var dbFile = "scan.db"
+
+type port struct {
+	Port    int    `json:"port"`
+	Proto   string `json:"proto"`
+	Status  string `json:"status"`
+	Service struct {
+		Name   string `json:"name"`
+		Banner string `json:"banner"`
+	} `json:"service"`
+}
+
+// Results posted from masscan
+type result struct {
+	IP    string `json:"ip"`
+	Ports []port `json:"ports"`
+}
+
+// Data retrieved from the database for display
+type scandata struct {
+	IP        string
+	Port      int
+	Proto     string
+	FirstSeen string
+	LastSeen  string
+	New       bool
+}
+
+// Load all data for displaying in the browser
+func load(s string) ([]scandata, error) {
+	db, err := sql.Open("sqlite3", dbFile)
+	if err != nil {
+		return []scandata{}, err
+	}
+	defer db.Close()
+
+	var where string
+	if s != "" {
+		where = `WHERE ip LIKE ?`
+		s = fmt.Sprintf("%%%s%%", s)
+	}
+
+	qry := fmt.Sprintf(`SELECT ip, port, proto, firstseen, lastseen FROM scan %s ORDER BY port, proto, ip, lastseen`, where)
+	rows, err := db.Query(qry, s)
+	if err != nil {
+		return []scandata{}, err
+	}
+
+	defer rows.Close()
+
+	var data []scandata
+	var ip, proto, firstseen, lastseen string
+	var port int
+
+	for rows.Next() {
+		err := rows.Scan(&ip, &port, &proto, &firstseen, &lastseen)
+		if err != nil {
+			return []scandata{}, err
+		}
+		f, _ := time.Parse("2006-01-02 15:04", firstseen)
+		l, _ := time.Parse("2006-01-02 15:04", lastseen)
+		data = append(data, scandata{ip, port, proto, firstseen, lastseen, l.Equal(f)})
+	}
+
+	return data, nil
+}
+
+// Save the results posted
+func save(results []result) error {
+	db, err := sql.Open("sqlite3", dbFile)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	txn, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	insert, err := txn.Prepare(`INSERT INTO scan (ip, port, proto, firstseen, lastseen) VALUES (?, ?, ?, ?, ?)`)
+	if err != nil {
+		txn.Rollback()
+		return err
+	}
+	qry, err := db.Prepare(`SELECT 1 FROM scan WHERE ip=? AND port=? AND proto=?`)
+	if err != nil {
+		txn.Rollback()
+		return err
+	}
+	update, err := txn.Prepare(`UPDATE scan SET lastseen=? WHERE ip=? AND port=? AND proto=?`)
+	if err != nil {
+		txn.Rollback()
+		return err
+	}
+
+	now := time.Now()
+	nowString := now.Format("2006-01-02 15:04")
+
+	for _, r := range results {
+		// Although it's an array, only one port is in each
+		port := r.Ports[0]
+
+		// Search for the IP/port/proto combo
+		// If it exists, update `lastseen`, else insert a new record
+
+		// Because we have to scan into something
+		var x int
+		err := qry.QueryRow(r.IP, port.Port, port.Proto).Scan(&x)
+		switch {
+		case err == sql.ErrNoRows:
+			_, err = insert.Exec(r.IP, port.Port, port.Proto, nowString, nowString)
+			if err != nil {
+				txn.Rollback()
+				return err
+			}
+			continue
+		case err != nil:
+			txn.Rollback()
+			return err
+		}
+
+		_, err = update.Exec(nowString, r.IP, port.Port, port.Proto)
+		if err != nil {
+			txn.Rollback()
+			return err
+		}
+	}
+
+	txn.Commit()
+	return nil
+}
+
+// Template is a template
+type Template struct {
+	templates *template.Template
+}
+
+// Render renders template
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+// Handler for GET /
+func index(c echo.Context) error {
+	ip := c.QueryParam("ip")
+	data, err := load(ip)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+	}
+	return c.Render(http.StatusOK, "index", data)
+}
+
+// Handler for GET /ips.json
+func ips(c echo.Context) error {
+	data, err := load("")
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+	}
+	var ips []string
+	for _, r := range data {
+		ips = append(ips, r.IP)
+	}
+	return c.JSON(http.StatusOK, ips)
+}
+
+// Handler for POST /results
+func recvResults(c echo.Context) error {
+	res := new([]result)
+	err := c.Bind(res)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	err = save(*res)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.NoContent(http.StatusOK)
+}
+
+func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+
+	e := echo.New()
+	e.Renderer = t
+	e.Use(middleware.Logger())
+	e.GET("/", index)
+	e.GET("/ips.json", ips)
+	e.POST("/results", recvResults)
+	e.Static("/static", "static")
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
