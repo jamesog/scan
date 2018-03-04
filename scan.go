c0d9d05dc47958d0e69be1338c229449fee3db92
James O'Gorman <james@netinertia.co.uk>
2018-03-04 18:06:02 +0000

    Use custom http.Server
    This switches to using a custom http.Server for each listener. This sets
    read/write/idle timeouts, and for TLS ensure we only use a modern TLS
    version and cipher suites.

--- scan.go
+++ scan.go
@@ -803,14 +803,43 @@ func main() {
 		r.Use(redirectHTTPS)
 	}
 
+	httpSrv := &http.Server{
+		Addr:         *httpAddr,
+		Handler:      r,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
 
 	if *enableTLS {
-		s := &http.Server{
-			Addr:      httpsAddr,
-			TLSConfig: &tls.Config{GetCertificate: m.GetCertificate},
+		httpsSrv := &http.Server{
+			Addr:         httpsAddr,
+			Handler:      r,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 5 * time.Second,
+			IdleTimeout:  120 * time.Second,
+
+			TLSConfig: &tls.Config{
+				GetCertificate:           m.GetCertificate,
+				PreferServerCipherSuites: true,
+				CurvePreferences: []tls.CurveID{
+					tls.CurveP256,
+					tls.X25519,
+				},
+				MinVersion: tls.VersionTLS12,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				},
+			},
 		}
-		go func() { log.Fatal(http.ListenAndServe(*httpAddr, r)) }()
-		log.Fatal(s.ListenAndServeTLS("", ""))
+		go func() { log.Fatal(httpSrv.ListenAndServe()) }()
+		log.Fatal(httpsSrv.ListenAndServeTLS("", ""))
 	}
-	log.Fatal(http.ListenAndServe(*httpAddr, r))
+
+	log.Fatal(httpSrv.ListenAndServe())
 }
